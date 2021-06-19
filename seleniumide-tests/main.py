5c5ad515b6d1d98a2a02190991ca1936b84b2723
Sirok Eszter <sirokeszter@gmail.com>
2021-06-19 14:21:50 +0200

    Homework - Selenium side files

---
+++ seleniumide-tests/main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("http://localhost:1667")
