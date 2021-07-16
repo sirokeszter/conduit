62a40de2a3a252a7a0e1b663f90a93503b354718
Sirok Eszter <sirokeszter@gmail.com>
2021-07-16 15:24:15 +0200

    REMEK.

---
+++ test_mappa/modify_data.py
@@ -0,0 +1,85 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import csv
+
+options = Options()
+# options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+
+try:
+    driver.get("http://localhost:1667/#/")
+    # Cookie accept:
+    button_accept = driver.find_element_by_xpath('//*[@id="cookie-policy-panel"]/div/div[2]/button[2]').click()
+
+    # Activate Sign in input field:
+    login = driver.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[2]/a')
+    time.sleep(3)
+    login.click()
+
+
+    # Fill input fields:
+    def fill_login(mail, pw):
+        email = driver.find_element_by_xpath('//*[@id="app"]//fieldset[1]/input')
+        password = driver.find_element_by_xpath('//*[@id="app"]//fieldset[2]/input')
+        button = driver.find_element_by_xpath('//*[@id="app"]//form/button')
+
+        email.send_keys(mail)
+        password.send_keys(pw)
+        button.click()
+
+
+    fill_login("kiskacsa7@gmail.com", "Kiskacsa7$")
+
+    time.sleep(3)
+
+    user_page = driver.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[4]/a').click()
+    time.sleep(2)
+    edit_profile=driver.find_element_by_xpath('//*[@id="app"]/div/div[1]//a').click()
+    time.sleep(3)
+
+    def profile_edition(photo,username, bio,email,password):
+        profile_photo=driver.find_element_by_xpath('//*[@id="app"]//fieldset[1]/input')
+        profile_username=driver.find_element_by_xpath('//*[@id="app"]//fieldset[2]/input')
+        profile_bio = driver.find_element_by_xpath('//*[@id="app"]//fieldset[3]/textarea')
+        profile_email = driver.find_element_by_xpath('//*[@id="app"]//fieldset[4]/input')
+        profile_password = driver.find_element_by_xpath('//*[@id="app"]//fieldset[5]/input')
+        update_button=driver.find_element_by_xpath('//*[@id="app"]//fieldset/button')
+
+
+        profile_photo.clear()
+        profile_photo.send_keys(photo)
+        profile_username.clear()
+        profile_username.send_keys(username)
+        profile_bio.clear()
+        profile_bio.send_keys(bio)
+        profile_email.clear()
+        profile_email.send_keys(email)
+        profile_password.clear()
+        profile_password.send_keys(password)
+        update_button.click()
+
+
+    profile_edition("https://senior.hu/wp-content/uploads/2019/01/eltuntek-a-tyukok-6.jpg", "tyukanyo", "Anyám tyúkja", "tyukanyo@gmail.com", "Tyukanyo123$")
+
+    time.sleep(3)
+    confirm_button = driver.find_element_by_xpath('/html/body/div[2]//button')
+    ref_text = driver.find_element_by_xpath('/html/body/div[2]/div/div[3]').text
+    print(ref_text)
+    # Checking correct alert messages coming...
+    confirm_button.click()
+
+    # Checking modified user name:
+    user_page = driver.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[4]/a').click()
+    time.sleep(2)
+    user_name = driver.find_element_by_xpath('//*[@id="app"]/div/div[1]/div/div/div/h4').text
+    time.sleep(2)
+    print(driver.current_url)
+    if driver.current_url == f"http://localhost:1667/#/@{user_name}/":
+        print('The modified username is: ', f"{user_name}")
+
+finally:
+    pass
+    # driver.close()
