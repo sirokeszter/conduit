24545f595d17fc787765fe838dc53ba716e380ec
Sirok Eszter <sirokeszter@gmail.com>
2021-07-23 19:04:28 +0200

    Masterpiece

--- exam_masterpiece/test_ts0_cookies.py
+++ exam_masterpiece/test_ts0_cookies.py
@@ -1,29 +1,30 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.common.exceptions import NoSuchElementException
+def test_cookies():
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    from webdriver_manager.chrome import ChromeDriverManager
+    from selenium.common.exceptions import NoSuchElementException
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
 
-try:
-    driver.get("http://localhost:1667/")
-    # Cookie accept:
-    button_accept = driver.find_element_by_xpath('//*[@id="cookie-policy-panel"]/div/div[2]/button[2]').click()
+    try:
+        driver.get("http://localhost:1667/")
+        # Cookie accept:
+        button_accept = driver.find_element_by_xpath('//*[@id="cookie-policy-panel"]/div/div[2]/button[2]').click()
 
-    # Decline button:
-    # button_decline=driver.find_element_by_xpath('//*[@id="cookie-policy-panel"]/div/div[2]/button[1]/div)
+        # Decline button:
+        # button_decline=driver.find_element_by_xpath('//*[@id="cookie-policy-panel"]/div/div[2]/button[1]/div)
 
-    def check_non_exists_by_xpath(xpath):
-        try:
-            driver.find_element_by_xpath(xpath)
-        except NoSuchElementException:
-            return True
-        return False
+        def check_non_exists_by_xpath(xpath):
+            try:
+                driver.find_element_by_xpath(xpath)
+            except NoSuchElementException:
+                return True
+            return False
 
-    check_non_exists_by_xpath('//*[@id="cookie-policy-panel"]/div/div[2]/button[2]')
+        check_non_exists_by_xpath('//*[@id="cookie-policy-panel"]/div/div[2]/button[2]')
 
-finally:
-    driver.close()
+    finally:
+        driver.close()
